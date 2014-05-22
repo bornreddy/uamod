c7f053f03d0578c4da39c3651641e89599ca92e9
bornreddy <marisa.reddy@gmail.com>
2014-05-22 18:11:19 -0400

    working on date now

--- app.js
+++ app.js
@@ -9,7 +9,8 @@
  var http = require('http');
  var path = require('path');
  parse = require('jsonml').parse;
- var parseString = require('xml2js').parseString;
+ var moment = require('moment');
+
  
 
  var app = express();
@@ -39,6 +40,11 @@ http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
 
+
+//----start of the code to get Ursuline Academy info, figure out how to put this into a dfferent file 
+//(http://scotch.io/tutorials/javascript/node-and-angular-to-do-app-application-organization-and-structure)
+
+
 //goals: find which items match the date of today, find the letter day, and find the schedule
 var xml = ""
 http.get("http://www.ursulineacademy.org/data/calendar/rsscache/page_357.rss", function(res) {
@@ -52,13 +58,27 @@ http.get("http://www.ursulineacademy.org/data/calendar/rsscache/page_357.rss", f
 		console.log(parse(xml));
     })*/
     res.on('end', function(){
-      if (xml.indexOf("<item>") != -1) {
+      while (xml.indexOf("<item>") != -1) {
       	i1 = xml.indexOf("<item>")
       	i2 = xml.indexOf("</item>")
-
+      	xml = xml.substring(i1, xml.length)
+      	
       	t1 = xml.indexOf("<title>") + 7
       	t2 = xml.indexOf("</title>")
-      	console.log(xml.substring(t1, t2))
+      	title = xml.substring(t1, t2)
+      	// console.log("title: " + title)
+      	// xml = xml.substring(t2+7, xml.length)
+      	l1 = xml.indexOf("<link>") + 6
+      	l2 = xml.indexOf("</link>")
+      	link = xml.substring(l1, l2)
+      	// console.log("link: " + link)
+      	// xml = xml.substring(l2+6, xml.length)
+      	d1 = xml.indexOf("<description>") + 13
+      	d2 = xml.indexOf("</description>")
+      	description = xml.substring(d1, d2)
+      	console.log("description: " + description)
+      	xml = xml.substring(d2+13, xml.length)
+
       }
     })
 }).on('error', function(e) {
